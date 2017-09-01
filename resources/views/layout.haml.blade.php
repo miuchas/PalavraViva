784c504ce29e99462da28e0dad9aaefc4264cbe2
phelipe <phelipe@task.com.br>
2017-09-01 15:21:48 -0300

    adicionando arquivos necessarios

--- resources/views/layout.haml.blade.php
+++ resources/views/layout.haml.blade.php
@@ -1,9 +1,10 @@
 !!!
-%html
+%html{:lang => "en"}
   %head
-    %meta{:charset => "utf-8"}
-    %meta{:content => "IE=edge", "http-equiv" => "X-UA-Compatible"}
-    %meta{:content => "width=device-width, initial-scale=1", :name => "viewport"}
-    %title Projeto Palavra Viva
+    %title PalavraViva
   %body
-    teste lindo
+    @include('sidebar')
+    @include('topo')
+    .page-container
+
+      @yield('corpo')
