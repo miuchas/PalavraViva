7ee0c439f767775a3703eb4576a46eb412ab816a
phelipe <phelipe@task.com.br>
2017-09-18 08:20:46 -0300

    definindo home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home.home');
     }
 
     public function logout(Request $request)
