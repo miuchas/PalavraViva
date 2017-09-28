c0a00c30a4768e5cc98295b025067bffdb55b9bc
phelipe <phelipe@task.com.br>
2017-09-28 10:56:19 -0300

    adicionar foto a listagem de requisição

--- resources/views/turma/lista_troca_turma.haml.blade.php
+++ resources/views/turma/lista_troca_turma.haml.blade.php
@@ -19,18 +19,22 @@
         .panel-body
           -foreach( $alerts as $alert )
             .lista{ :style => "position: relative;" }
-              %a.list-group-item{:href => "/turma/aprovar-troca-turma/".$alert->id}
-                %span.contacts-title
+              %a.list-group-item{:href => "#"}
+                %span{ :style => "display: inline-block" }
+                  %img{ :src => @Auth::user()->imagem, :style => "max-width: 92px; margin-top: -14px; border: 1px solid #E5E5E5; border-radius: 4px;"}
+                %span.contacts-title{ :style => "display: inline-block" }
                   %strong
                     =$alert->tipo
-                %p
-                  =$alert->descricao_basica
-                -if ( $alert->obs != "" )
                   %p
-                    Obs.:
-                    =$alert->obs
+                    =$alert->descricao_basica
+                  -if ( $alert->obs != "" )
+                    %p
+                      Obs.:
+                      =$alert->obs
               .list-group-controls
-                %a.btn.btn-danger.btn-rounded{:href => "/turma/remove-troca-turma/".$alert->id, :style => "margin: 5px;"}
+                %a.btn.btn-success.btn-rounded{:href => "/turma/aprovar-troca-turma/".$alert->id, :style => "margin-top: 28px;", "data-toggle" => "tooltip", "data-placement" => "bottom", :title => "Aprovar troca de turma" }
+                  %span.fa.fa-check{ :style => "margin-right: 0px;" }
+                %a.btn.btn-danger.btn-rounded{:href => "/turma/remove-troca-turma/".$alert->id, :style => "margin-top: 28px; margin-right: 10px;", "data-toggle" => "tooltip", "data-placement" => "bottom", :title => "Excluir requisição" }
                   %span.fa.fa-trash-o{ :style => "margin-right: 0px;" }
 
         .panel-footer
